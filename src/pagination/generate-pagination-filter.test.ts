7c331082e992180384e6d319734d81fa1f7c906f
Rasim Sen <rasimsen@gmail.com>
2022-10-18 16:46:08 +0100

    jest-unit test

--- src/pagination/generate-pagination-filter.test.ts
+++ src/pagination/generate-pagination-filter.test.ts
@@ -1,5 +1,23 @@
+import 'reflect-metadata'
+import { ENUM_FILTER_OPERATOR_TYPE } from "../common.types";
+import { generatePaginationFilter } from "./generate-pagination-filter";
+import { filterDto } from "./pagination.dto";
+
 describe('pagination filter tests', () => {
     test('between test', () => {
-        expect("test").toBe("test");
+        const filter: filterDto[] = [
+            {
+                "name": "schedule.eventStartDateTime",
+                "arr_value": ["2032-12-15", "2032-12-15"],
+                "operator": ENUM_FILTER_OPERATOR_TYPE.between,
+                value: null,
+                mode: null
+            }
+        ]
+
+        const fr = generatePaginationFilter(filter)
+        expect(fr).toEqual({
+            'schedule.eventStartDateTime': { '$gte': '2032-12-15', '$lte': '2032-12-15' }
+        });
     });
 });
